93e8bef6c3a9220087a2cf282d2a453c211890af
adams <ghostry@ghostry.cn>
2021-03-09 23:49:51 +0800

    删除无用文件

--- up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git remote add upstream https://github.com/P3TERX/Aria2-Pro-Docker.git
-git remote -v
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push origin master
